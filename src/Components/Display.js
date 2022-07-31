ed83a0c63ca5bc5fd4363d0d2a9cabd16ff7f8db
Joel Aguirre <joelaguirre@Joels-MacBook-Pro.local>
2022-07-31 13:09:23 -0500

    slight changes

--- src/Components/Display.js
+++ src/Components/Display.js
@@ -1,18 +1,53 @@
-import { useState } from "react"
+
+import { useState, useEffect } from "react"
 
 
 function Display({  }) {
     const [product, setProduct] = useState([])
-
+    const [favorites, setFavorites] = useState([])  
+            const handleFavorites = async () => {
+                const response = await fetch(`https://wisc-wish-list-app.herokuapp.com/favorites/user-favorites/name/joel`)
+                
+                const data = await response.json()
+                console.log(data.favorites)
+                
+                
+                setFavorites(data.favorites)
+                console.log(favorites)
+                
+            }
+            useEffect(() => {handleFavorites()}, [])
     const handleChange = (e) => {
         setProduct(e.target.value)
        }
+
+    const addFavorites = () => {
+        favorites.push(product)
+        console.log(favorites)
+        console.log(product)
+        
+        
+        
+        const requestFavorites = {
+            headers: {
+                mode:'no-cors',
+                'Content-Type': 'application/json'
+                
+            },
+            method: 'PUT',
+            body: JSON.stringify(favorites)
+        };
+        fetch('https://wisc-wish-list-app.herokuapp.com/favorites/user-favorites/name/joel', requestFavorites)
+        .then(response => console.log('saved'))
+            
+    }
+    
        const clear = () => {
             setProduct(null)
        }
     const handleSubmit = async (e) => {
         e.preventDefault()
-        const response = await fetch(`https://api.bestbuy.com/v1/products/${product}.json?show=sku,name,regularPrice,manufacturer,salePrice,image,url,addToCartUrl,shortDescription&apiKey=${process.env.REACT_APP_MY_BBKEY}`)
+        const response = await fetch(`https://api.bestbuy.com/v1/products/${product}.json?show=sku,url,shortDescription,salePrice,regularPrice,name,manufacturer,image&apiKey=${process.env.REACT_APP_MY_BBKEY}`)
         const data = await response.json()
         console.log(data)
         
@@ -30,17 +65,20 @@ function Display({  }) {
         <span className="placeholder col-3">
             <input className="form-control me-4" onChange={handleChange}placeholder="product"/></span>
                 <button className="btn btn-outline-success" >Search</button>
-                <button className="btn btn-outline-success">Add to WishList</button>
+                
     </form>
         
         <div className="container-fluid col-8">
               <h1>{product.name}</h1>
               <img src = {product.image} alt='product'/>
-              <h2>Retail Price ${product.regularPrice} </h2>
-              <h2>Sale Price ${product.salePrice}</h2>
+              <h2>Was ${product.regularPrice} </h2>
+              <h2>Now Price ${product.salePrice}</h2>
               <h3>{product.shortDescription}</h3>
-              {/* <a href={product.addToCartUrl} target='blank'>
-              <h3>Add</h3></a> */}
+             
+              <a href={product.addToCartUrl} target="blank">add to cart</a>
+              <br/>
+              
+                <button className="btn btn-primary" onClick={addFavorites}>Add to Favorites</button>
         </div>
         </nav>
         
--- src/Components/favorites.js
+++ src/Components/favorites.js
@@ -1,11 +1,12 @@
 import React, { useState, useEffect } from 'react';
 
+
  
 
     function Favorites({ }) {
         const [favorites, setFavorites] = useState([])  
             const handleSubmit = async () => {
-                const response = await fetch(`http://localhost:4050/favorites/user-favorites/name/joel`)
+                const response = await fetch(`https://wisc-wish-list-app.herokuapp.com/favorites/user-favorites/name/joel`)
                 
                 const data = await response.json()
                 console.log(data.favorites)
@@ -16,7 +17,21 @@ import React, { useState, useEffect } from 'react';
                 
             }
                     
-                
+                const deleteFavorite = Index => ()=> {
+                    console.log(Index)
+                   favorites.splice(Index, 1)
+                    const requestFavorites = {
+                        headers: {
+                            mode:'no-cors',
+                            'Content-Type': 'application/json'
+                            
+                        },
+                        method: 'PUT',
+                        body: JSON.stringify(favorites)
+                    };
+                    fetch('https://wisc-wish-list-app.herokuapp.com/favorites/user-favorites/name/joel', requestFavorites)
+                    .then(response => window.location.reload())
+                }
                 
                 
                
@@ -24,17 +39,25 @@ import React, { useState, useEffect } from 'react';
                 let faves = []
                 for(let i = 0; i<favorites.length; i++) {
                     faves.push(
-                        <div key = {i} className="container-fluid col-8">
-                        <h1>{favorites[i]?.name}</h1>
-                        <img src = {favorites[i]?.image} alt='product'/>
-                        <h2>Retail Price ${favorites[i]?.regularPrice} </h2>
-                        <h2>Sale Price ${favorites[i]?.salePrice}</h2>
-                        <h3>{favorites[i]?.shortDescription}</h3>
-                        <a href={favorites[i]?.addToCartUrl} target='blank'>
-                         <h3>Add to Wishlist</h3></a>
-            </div>
+                            
+                        <div key = {i} className="container">
+                            <div className="row">
+                                
+                                <article className="u-shadow-v18 g-bg-white text-center rounded g-px-20 g-py-40 g-mb-5">
+                                <img className="d-inline-block img-fluid mb-4" src = {favorites[i]?.image} alt='product'/>
+                                <h4 className="h5 g-color-black g-font-weight-600 g-mb-10">{favorites[i]?.name}</h4>
+                                <p>My favorite Products</p>
+                                <span className="d-block g-color-primary g-font-size-16">Retail Price ${favorites[i]?.regularPrice}</span>
+                                <span className="d-block g-color-primary g-font-size-16">Sale Price ${favorites[i]?.salePrice}</span>
+                                </article>
+                                    <button onClick={deleteFavorite(i)} className="btn btn-primary">Delete</button>
+                            </div>
+                        
+                        </div>
                     )
                 }
+
+
                 
                 return (
                     
